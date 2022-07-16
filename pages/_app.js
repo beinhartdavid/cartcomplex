3f915e42695dd430bb6bda31e4a6131e401333e2
David <beinhart.david@gmail.com>
2022-07-16 19:26:13 -0400

    index page is working need to add api route auth

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,16 @@
 
 import '../styles/index.css';
 import {TodosProvider} from "../contexts/TodosContext";
+import { UserProvider } from '@auth0/nextjs-auth0';
 function MyApp({ Component, pageProps }) {
     return (
+      <UserProvider>
       <TodosProvider>
-        
             <div className="container mx-auto my-10 max-w-xl">
                 <Component {...pageProps} />
             </div>
             </TodosProvider>
+            </UserProvider>
     );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import Head from 'next/head';
 import { table, minifyRecords } from './api/utils/airtable';
 import Todo from '../components/Todo';
-import Navbar from '../components/Navbar';
+import { useEffect, useContext } from 'react';
 import { TodosContext } from '../contexts/TodosContext';
-import { useEffect,useContext } from 'react';
-export default function Home({ initialTodos }) {
-  const {todos,setTodos} = useContext(TodosContext);
-  useEffect(()=> {
-    setTodos(initialTodos)
-  },[]);
+import TodoForm from '../components/TodoForm';
+import { getSession } from '@auth0/nextjs-auth0';
+
+export default function Home({ initialTodos, user }) {
+    const { todos, setTodos } = useContext(TodosContext);
+    useEffect(() => {
+        setTodos(initialTodos);
+    }, []);
 
     return (
         <div className="max-w-xl m-auto p-2">
@@ -17,27 +19,64 @@ export default function Home({ initialTodos }) {
             </Head>
 
             <main>
-                <Navbar />
-                <>
-                    <ul>
-                        {initialTodos &&
-                            initialTodos.map((todo) => (
-                                <Todo todo={todo} key={todo.id} />
-                            ))}
-                    </ul>
-                </>
+                <nav>
+                    <div className="flex items-center justify-between py-4  ">
+                        <div className="flex justify-between items-center">
+                            <div className="text-2xl font-bold text-gray-800 md:text-3xl">
+                                <a href="#">My Todos</a>
+                            </div>
+                        </div>
+                        <div className="flex">
+                            {user ? (
+                                <a
+                                    href="/api/auth/logout"
+                                    className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4"
+                                >
+                                    Logout
+                                </a>
+                            ) : (
+                                <a
+                                    href="/api/auth/login"
+                                    className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4"
+                                >
+                                    Login
+                                </a>
+                            )}
+                        </div>
+                    </div>
+                </nav>
+                {user ? (
+                    <>
+                        <TodoForm />
+                        <ul>
+                            {todos &&
+                                todos.map((todo) => (
+                                    <Todo todo={todo} key={todo.id} />
+                                ))}
+                        </ul>
+                    </>
+                ) : (
+                    <p className="text-center mt-4">
+                        Please login to save todos!
+                    </p>
+                )}
             </main>
         </div>
     );
 }
 
 export async function getServerSideProps(context) {
-    let todos = await table
-        .select({ })
-        .firstPage();
+    const session = await getSession(context.req, context.res);
+    let todos = [];
+    if (session?.user) {
+        todos = await table
+            .select({ filterByFormula: `userId = '${session.user.sub}'` })
+            .firstPage();
+    }
     return {
         props: {
             initialTodos: minifyRecords(todos),
+            user: session?.user || null,
         },
     };
 }
